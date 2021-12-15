04dd97429dcc510d9d62d54534fcc32ab78f6c3f
Ricsko <szudiricsi@gmail.com>
2021-12-15 08:40:59 +0100

    nevellenorzes, palyageneralas(rosszhelyen van)

---
+++ sakk/sakk/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace sakk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.jatekos1TBOX = new System.Windows.Forms.TextBox();
+            this.jatekos2TBOX = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // jatekos1TBOX
+            // 
+            this.jatekos1TBOX.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.jatekos1TBOX.Location = new System.Drawing.Point(198, 94);
+            this.jatekos1TBOX.Name = "jatekos1TBOX";
+            this.jatekos1TBOX.Size = new System.Drawing.Size(127, 26);
+            this.jatekos1TBOX.TabIndex = 0;
+            // 
+            // jatekos2TBOX
+            // 
+            this.jatekos2TBOX.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.jatekos2TBOX.Location = new System.Drawing.Point(198, 140);
+            this.jatekos2TBOX.Name = "jatekos2TBOX";
+            this.jatekos2TBOX.Size = new System.Drawing.Size(127, 26);
+            this.jatekos2TBOX.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(94, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Player1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(94, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Player2:";
+            // 
+            // startBTN
+            // 
+            this.startBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.startBTN.Location = new System.Drawing.Point(355, 295);
+            this.startBTN.Name = "startBTN";
+            this.startBTN.Size = new System.Drawing.Size(143, 76);
+            this.startBTN.TabIndex = 5;
+            this.startBTN.Text = "Játék indítása";
+            this.startBTN.UseVisualStyleBackColor = true;
+            this.startBTN.Click += new System.EventHandler(this.startBTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.startBTN);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.jatekos2TBOX);
+            this.Controls.Add(this.jatekos1TBOX);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox jatekos1TBOX;
+        private System.Windows.Forms.TextBox jatekos2TBOX;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button startBTN;
+    }
+}
+
---
+++ sakk/sakk/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sakk
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        static PictureBox[,] jatekter = new PictureBox[8, 8];
+        static PictureBox aktBabu;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startBTN_Click(object sender, EventArgs e)
+        {
+            nevellenorzes();
+            Size = new Size(1920, 1080);
+        }
+
+        private void nevellenorzes()
+        {
+           /* string nev1 = jatekos1TBOX.Text;
+            string nev2 = jatekos2TBOX.Text;
+
+            if(nev1 == "" || nev2 == "")
+            {
+                MessageBox.Show("Adja meg a játékosok nevét!");
+            }
+            else
+            {
+               
+            }*/
+
+            gombeltuntetes();
+            jatektergeneralas();
+        }
+
+        private void jatektergeneralas()
+        {
+            int x = this.ClientSize.Width / 2;
+            int y = this.ClientSize.Height / 2;
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    PictureBox palya = new PictureBox();
+                    palya.Size = new Size(100, 100);
+                    palya.Location = new Point(x + 6, y);
+                    palya.Name = $"{i}_{j}";
+                   // palya.MouseClick += new MouseEventHandler(mozgatasClick);
+                    if ((i + j) % 2 == 0)
+                    {
+                        palya.BackColor = Color.FromArgb(255, 216, 176);
+                    }
+                    else
+                    {
+                        palya.BackColor = Color.FromArgb(69, 40, 36);
+                        palya.Enabled = false;
+                    }
+                    this.Controls.Add(palya);
+                    jatekter[i, j] = palya;
+
+                    x += 100;
+                }
+
+                x = this.ClientSize.Width / 2;
+                y += 100;
+            }
+            aktBabu = new PictureBox();
+
+        }
+
+        private void gombeltuntetes()
+        {
+            label1.Visible = false;
+            label2.Visible = false;
+            jatekos1TBOX.Visible = false;
+            jatekos2TBOX.Visible = false;
+            startBTN.Visible = false;
+        }
+    }
+}
